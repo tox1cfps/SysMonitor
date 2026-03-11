cb748f50d3e2906d2822e94f5f9845d5d51ae541
Arthur Rodrigues Fonseca <95777078+tox1cfps@users.noreply.github.com>
2026-03-11 10:28:23 -0300

    Implement user prompt for terminating processes
    Added a loop to prompt the user for process termination and handle input validation.

--- main.py
+++ main.py
@@ -92,6 +92,48 @@ def process_info():
                 f"| RAM: {proc['memory_percent']:.2f}%"
             )
 
+    while True:
+        print("\nDeseja encerrar algum processo?")
+
+        print("\n[1] - Sim")
+        print("\n[2] - Não")
+
+        try:
+            opcao = int(input("\nSelecione sua opção: "))
+        except ValueError:
+            print("Digite um número válido.")
+            continue
+
+        if opcao == 1:
+            pid = input("Digite o número PID do processo: ")
+            try:
+                int_pid = int(pid)
+                end_process(int_pid)
+                break
+            except ValueError:
+                print("Erro: O PID deve ser um número inteiro.")
+        if opcao == 2:
+            break
+
+def end_process(process_pid):
+    for proc in psutil.process_iter(['pid', 'name']):
+        try:
+            if process_pid == proc.info['pid']:
+                print(f"Encontrado: {proc.info['name']} (PID: {proc.info['pid']})")
+                
+                proc.terminate()
+
+                gone, alive = psutil.wait_procs([proc], timeout=3)
+
+                if alive:
+                    print(f"Processo {proc.info['pid']} não respondeu, forçando Kill...")
+                    proc.kill()
+                else:
+                    print("Processo encerrado com sucesso")
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
 def internet_info():
     net1 = psutil.net_io_counters()
     time.sleep(1)
